b944f0e3ddc0e577a371eaf25286f903a0e63ae5
Mahmoud Ahmed El Sayed El Ruby <hodaroby1987@gmail.com>
2025-02-15 11:48:58 +0200

    create custom appBar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_app_bar.dart';
+
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: NotesViewBody(),
+    );
   }
 }
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+          
+            CustomAppBar(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
