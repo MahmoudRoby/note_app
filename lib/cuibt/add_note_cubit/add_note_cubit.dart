21065fc9bc71df39ddae677bdaffee5e3114bc9b
Mahmoud Ahmed El Sayed El Ruby <hodaroby1987@gmail.com>
2025-02-18 17:12:33 +0200

    color list

--- lib/cuibt/add_note_cubit/add_note_cubit.dart
+++ lib/cuibt/add_note_cubit/add_note_cubit.dart
@@ -9,16 +9,16 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-
+  Color color = Colors.green;
   addNote(NoteModel note) async {
     emit(AddNoteLoading());
     try {
+      note.color = color.value;
       var noteBox = Hive.box<NoteModel>(kNotesBox);
       await noteBox.add(note);
       emit(AddNoteSuccess());
     } catch (e) {
       emit(AddNoteFailure(e.toString()));
-      
     }
   }
 }
--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:note_app/cuibt/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app/cuibt/notes/notes_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/widgets/custom_button.dart';
 import 'package:note_app/widgets/custom_text_field.dart';
@@ -51,13 +52,18 @@ class _AddFormState extends State<AddForm> {
           const SizedBox(
             height: 24,
           ),
+          const ColorListView(),
+          const SizedBox(
+            height: 24,
+          ),
           BlocBuilder<AddNoteCubit, AddNoteState>(builder: (context, state) {
             return CustomButton(
               isLoading: state is AddNoteLoading ? true : false,
               onTap: () {
                 if (formKey.currentState!.validate()) {
                   formKey.currentState!.save();
-                  var dateFormate = DateFormat('dd-mm-yy hh:mm').format(DateTime.now());
+                  var dateFormate =
+                      DateFormat('dd-mm-yy hh:mm').format(DateTime.now());
                   BlocProvider.of<AddNoteCubit>(context).addNote(NoteModel(
                       title: title!,
                       subtitle: subtitle!,
@@ -74,6 +80,68 @@ class _AddFormState extends State<AddForm> {
             height: 5,
           )
         ],
+      ),
+    );
+  }
+}
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key, required this.isSelected, required this.color});
+  final bool isSelected;
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return isSelected
+        ? CircleAvatar(
+            backgroundColor: color,
+            radius: 40,
+          )
+        : CircleAvatar(
+            backgroundColor: color,
+            radius: 30,
+          );
+  }
+}
+
+class ColorListView extends StatefulWidget {
+  const ColorListView({super.key});
+
+  @override
+  State<ColorListView> createState() => _ColorListViewState();
+}
+
+class _ColorListViewState extends State<ColorListView> {
+  int currentIndex = 0;
+  List<Color> kColors = const [
+    Color(0xffAC3931),
+    Color(0xffE5D352),
+    Color(0xff537D8D),
+    Color(0xffD9E76C),
+    Color(0xff482C3D),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: kColors.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              currentIndex = index;
+              BlocProvider.of<AddNoteCubit>(context).color = kColors[index];
+              setState(() {});
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: ColorItem(
+                color: kColors[index],
+                isSelected: currentIndex == index,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
